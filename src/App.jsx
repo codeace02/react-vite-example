f1febcf29349c790be0b1e67541e1003ff0244f5
Amisha <amisha9565@gmail.com>
2024-09-16 11:37:24 +0530

    comment

--- src/App.jsx
+++ src/App.jsx
@@ -10,7 +10,7 @@ function App() {
       {/* <Card /> */}
       {/* <BgChanger /> */}
       {/* <PasswordGenerator /> */}
-      <CurrencyConvertor />
+      {/* <CurrencyConvertor /> */}
     </>
   )
 }
