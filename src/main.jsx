630bfc5324e5a7ceafcf304696a285c040509ac0
Amisha <amisha9565@gmail.com>
2024-09-16 11:38:46 +0530

    implementation of react-router-dom & route/nested route, params, loading components(api fetch during load time)

--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,60 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx';
 import "./index.css"
+import { createBrowserRouter, createRoutesFromElements, Route, RouterProvider } from 'react-router-dom';
+import Layout from './components/react-router/Layout/Layout.jsx';
+import Home from './components/react-router/Home.jsx';
+import About from './components/react-router/About.jsx';
+import Contact from './components/react-router/Contact.jsx';
+import Github, { githubInfoLoader } from './components/react-router/Github.jsx';
+import User from './components/react-router/User.jsx';
+
+// const router = createBrowserRouter([
+//   {
+//     path: '/',
+//     element: <Layout />,
+//     children: [
+//       {
+//         path: '',
+//         element: <Home />
+//       },
+//       {
+//         path: 'about',
+//         element: <About />
+//       },
+//       {
+//         path: 'contact',
+//         element: <Contact />
+//       },
+//       {
+//         path: 'github',
+//         element: <Github />
+//       }
+//     ]
+//   }
+// ])
+
+// other way
+
+const router2 = createBrowserRouter(
+  createRoutesFromElements(
+    <Route path='/' element={<Layout />}>
+      <Route path='' element={<Home />} />
+      <Route path='about' element={<About />} />
+      <Route path='contact' element={<Contact />} />
+      <Route path='user/:userId' element={<User />} />
+      <Route
+        loader={githubInfoLoader}
+        path='github'
+        element={<Github />}
+      />
+    </Route>
+  )
+)
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <RouterProvider router={router2} />
+    {/* <App /> */}
   </StrictMode>,
 )
