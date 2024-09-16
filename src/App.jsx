31339637b930e3827027aec8546da62d74d397e2
Amisha <amisha9565@gmail.com>
2024-09-16 18:56:51 +0530

    Theme changer

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,4 @@
+import { useState } from "react";
 import BgChanger from "./components/BgChanger"
 import Card from "./components/Card"
 import Login from "./components/context-example/user-example/Login";
@@ -5,6 +6,7 @@ import Profile from "./components/context-example/user-example/Profile";
 import CurrencyConvertor from "./components/currency-app/CurrencyConvertor"
 import PasswordGenerator from "./components/PasswordGenerator";
 import UserContextProvider from "./context/UserContextProvider";
+import ThemeChanger from "./components/context-example/theme-example/ThemeChanger";
 
 function App() {
 
@@ -15,10 +17,13 @@ function App() {
       {/* <PasswordGenerator /> */}
       {/* <CurrencyConvertor /> */}
 
-      <UserContextProvider>
+
+      {/* <UserContextProvider>
         <Login />
         <Profile />
-      </UserContextProvider>
+      </UserContextProvider> */}
+
+      <ThemeChanger />
     </>
   )
 }
